58ddb8dc34c25670324bb92e8ef8a08163eb9ea7
Alena <insolarance@seznam.cz>
2024-06-12 22:51:28 +0200

    Update master branch

---
+++ data/orderDetails.js
@@ -0,0 +1,33 @@
+export const orderDetails = {
+  email: "Tester",
+  firstName: "Jhon",
+  lastName: "Snow",
+  company: "Faag Corporation",
+  street: "Loneroad",
+  city: "Tokio",
+  state: "Alaska",
+  zip: "123 45",
+  country: "Cocos (Keeling) Islands",
+  phone: "444 444 444",
+};
+
+// //add beVisible(false) for notification that shouldnt appears
+
+// //api
+// import * as nodeFetch from "node-fetch"
+
+// export const getLogin = async () =>{
+//     const response = await nodeFetch("localhost", {
+//         method : "POST";
+//         body: { "username": "admon," },
+//     })
+//     if(response.status !== 200){
+//         throw new Error ("Error returning during trying to return token")
+//     }
+
+//     const body = await response.json();
+//     return body.token
+// }
+
+// import * as dotenv from "dotenv"
+// dotenv.config()
---
+++ objects/deleteFromCart.js
@@ -0,0 +1,7 @@
+// // create page objects file for all page objects
+// console.warn({allPriceText})
+// allPrice.map((element) => 
+// {
+//     constWithoutUSDSign = element.replace("$", "")
+//     return parseInt
+// })
